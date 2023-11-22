dc0f56791aea74ee3acd9780bdc395ef17547e69
nmquang1416 <nmquang1416@gmail.com>
2023-11-22 19:22:13 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const radio_rq_female = document.getElementById("female");
+const radio_rq_male = document.getElementById("male");
+
+const er-
+
+function error_radio() {
+    if (radio_rq_female = 0) {
+        er_fisrt_name.innerHTML('p')   
+    }
+}
